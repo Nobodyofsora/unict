f2109d3f62361272a1fff5302eee87747f32f3b6
Manuel Caruso <manuel.8@hotmail.it>
2020-05-30 20:06:44 +0200

    Feature Tweets GET & POST nobodyofsora (#5)
    * first commit
    
    * added tweet creation
    
    * added POST
    
    * fixed graphical imperfections

--- tests/tweets.spec.js
+++ tests/tweets.spec.js
@@ -1,97 +1,187 @@
 const chai = require('chai');
-const expect = require("chai").expect;
-const mongoose = require('mongoose');
-const app = require('../app');
 const chaiHttp = require('chai-http');
+const app = require('../app');
+const expect = chai.expect;
 const { expectJson, createTweet, createUser } = require('./utils/index');
 
+const Tweet = require('../models/tweet');
+const User = require('../models/user');
+const utils = require('./utils/index');
+
+const mongoose = require('mongoose');
 chai.use(chaiHttp);
 
-describe("[EDIT] PUT: /tweets/:id", () => {
+describe('[SHOW] \n Returns all tweets', () => {
+  it('should return lenght 0 if no tweets are found', async () => {
+    const result = await chai.request(app).get('/tweets');
+    expect(result.status).to.be.equal(200);
+    expect(result.body).to.be.instanceOf(Array);
+    expect(result.body).to.have.lengthOf(0);
+  });
 
-    let currentUser = undefined;
-    let currentTweet = undefined;
-
-    before("Create tweet", async () => {
-        currentUser = await createUser();
-        currentTweet = await createTweet(currentUser);
+  describe('If tweets exist', () => {
+    let tweet = undefined;
+    before('create tweet', async () => {
+      tweet = await utils.createTweet();
     });
-
     after('delete tweet', () => {
-        currentTweet ? currentTweet.remove() : console.log('missing tweet');
-        currentUser ? currentUser.remove() : console.log('missing user');
+      utils.deleteTweet(tweet);
     });
-
-    it("Edit tweet", async () => {
-        const result = await chai
-            .request(app)
-            .put(`/tweets/${currentTweet._id.toString()}`)
-            .send({ tweet: String(currentTweet.tweet).split("").reverse().join("") });
-
-        expectJson(result);
-        expect(result).to.have.property("status", 200);
-        expect(result.body).to.have.property("tweet", String(currentTweet.tweet).split("").reverse().join(""));
+    it(' should return lenght 1', async () => {
+      const result = await chai.request(app).get('/tweets');
+      expect(result.status).to.be.equal(200);
+      expect(result.body).to.be.instanceOf(Array);
+      expect(result.body).to.be.lengthOf(1);
     });
-
-    it("should return status 404 if tweetID not exist", async () => {
-        const result = await chai
-            .request(app)
-            .put(`/tweets/${mongoose.Types.ObjectId()}`)
-            .send({ tweet: "Hello world" });
-
-        expectJson(result);
-        expect(result).to.have.property("status", 404);
-    });
-
-    it("should return status 400 if tweet lenght is 0", async () => {
-        const result = await chai
-            .request(app)
-            .put(`/tweets/${mongoose.Types.ObjectId()}`)
-            .send({ tweet: "" });
-
-        expectJson(result);
-        expect(result).to.have.property("status", 400);
-    });
-
-    it("should return status 400 if tweet lenght is greater than 120", async () => {
-        const result = await chai
-            .request(app)
-            .put(`/tweets/${mongoose.Types.ObjectId()}`)
-            .send({ tweet: Array(140).join('x') });
-
-        expectJson(result);
-        expect(result).to.have.property("status", 400);
-    });
+  });
+  // describe('Server Crash', () => {
+  //   it('should return status code 500', async () => {
+  //     const result = await chai.request(app).get('/tweets');
+  //     expect(result.status).to.be.equal(500);
+  //   });
+  // });
 });
 
-describe("[DELETE] DELETE: /tweets/:id", () => {
-    let currentUser = undefined;
-    let currentTweet = undefined;
+describe('[SHOW] Returns a tweet', () => {
+  it('should return status 404 if tweet is missing', async () => {
+    const id = mongoose.Types.ObjectId();
+    const result = await chai.request(app).get(`/tweets/${id}`);
+    expect(result.status).to.be.equal(404);
+  });
+  describe('If tweet exists', () => {
+    let tweet = undefined;
+    before('create tweet', async () => {
+      tweet = await utils.createTweet();
+    });
+    after('delete tweet', () => {
+      utils.deleteTweet(tweet);
+    });
+    it('should return that tweet', async () => {
+      const result = await chai
+        .request(app)
+        .get(`/tweets/${tweet._id.toString()}`);
+      expect(result.status).to.be.equal(200);
+      expect(result.body).to.have.property('_id', tweet._id.toString());
+    });
+  });
+});
 
-    before("Create tweet", async () => {
-        currentUser = await createUser();
-        currentTweet = await createTweet(currentUser);
+describe('[CREATE] POST: /tweets', () => {
+  let tweet = undefined;
+  before('cleaning', () => {
+    utils.deleteAll();
+  });
+  after('delete tweet', async () => {
+    await Tweet.findByIdAndDelete(tweet._id);
+    await User.findByIdAndDelete(tweet._author);
+  });
+  it('should create a tweet', async () => {
+    const user = await User.create({
+      name: 'Manuel',
+      surname: 'Caruso',
+      email: 'manuel.caruso@stevejobs.academy',
+      password: 'pass',
     });
 
-    after("delete user", () => {
-        currentUser ? currentUser.remove() : console.log('missing user');
+    const result = await chai.request(app).post(`/tweets`).send({
+      _author: user._id,
+      tweet: 'Example',
     });
+    tweet = result.body;
 
-    it("Delete user", async () => {
-        const result = await chai
-            .request(app)
-            .delete(`/tweets/${currentTweet._id.toString()}`);
-        expectJson(result);
-        expect(result.body).to.have.property("message", 'Tweet successfully deleted');
-        expect(result).to.have.property("status", 200);
-    });
+    expect(result.status).to.be.equal(201);
+  });
+});
 
-    it("should return status 404 if tweetID not exist", async () => {
-        const result = await chai
-            .request(app)
-            .delete(`/tweets/${mongoose.Types.ObjectId()}`);
-        expectJson(result);
-        expect(result).to.have.property("status", 404);
-    });
+describe('[EDIT] PUT: /tweets/:id', () => {
+  let currentUser = undefined;
+  let currentTweet = undefined;
 
+  before('Create tweet', async () => {
+    currentUser = await createUser();
+    currentTweet = await createTweet(currentUser);
+  });
+
+  after('delete tweet', () => {
+    currentTweet ? currentTweet.remove() : console.log('missing tweet');
+    currentUser ? currentUser.remove() : console.log('missing user');
+  });
+
+  it('Edit tweet', async () => {
+    const result = await chai
+      .request(app)
+      .put(`/tweets/${currentTweet._id.toString()}`)
+      .send({ tweet: String(currentTweet.tweet).split('').reverse().join('') });
+
+    expectJson(result);
+    expect(result).to.have.property('status', 200);
+    expect(result.body).to.have.property(
+      'tweet',
+      String(currentTweet.tweet).split('').reverse().join(''),
+    );
+  });
+
+  it('should return status 404 if tweetID not exist', async () => {
+    const result = await chai
+      .request(app)
+      .put(`/tweets/${mongoose.Types.ObjectId()}`)
+      .send({ tweet: 'Hello world' });
+
+    expectJson(result);
+    expect(result).to.have.property('status', 404);
+  });
+
+  it('should return status 400 if tweet lenght is 0', async () => {
+    const result = await chai
+      .request(app)
+      .put(`/tweets/${mongoose.Types.ObjectId()}`)
+      .send({ tweet: '' });
+
+    expectJson(result);
+    expect(result).to.have.property('status', 400);
+  });
+
+  it('should return status 400 if tweet lenght is greater than 120', async () => {
+    const result = await chai
+      .request(app)
+      .put(`/tweets/${mongoose.Types.ObjectId()}`)
+      .send({ tweet: Array(140).join('x') });
+
+    expectJson(result);
+    expect(result).to.have.property('status', 400);
+  });
+});
+
+describe('[DELETE] DELETE: /tweets/:id', () => {
+  let currentUser = undefined;
+  let currentTweet = undefined;
+
+  before('Create tweet', async () => {
+    currentUser = await createUser();
+    currentTweet = await createTweet(currentUser);
+  });
+
+  after('delete user', () => {
+    currentUser ? currentUser.remove() : console.log('missing user');
+  });
+
+  it('Delete user', async () => {
+    const result = await chai
+      .request(app)
+      .delete(`/tweets/${currentTweet._id.toString()}`);
+    expectJson(result);
+    expect(result.body).to.have.property(
+      'message',
+      'Tweet successfully deleted',
+    );
+    expect(result).to.have.property('status', 200);
+  });
+
+  it('should return status 404 if tweetID not exist', async () => {
+    const result = await chai
+      .request(app)
+      .delete(`/tweets/${mongoose.Types.ObjectId()}`);
+    expectJson(result);
+    expect(result).to.have.property('status', 404);
+  });
 });
--- tests/utils/index.js
+++ tests/utils/index.js
@@ -2,26 +2,32 @@ const expect = require('chai').expect;
 const Tweet = require('../../models/tweet');
 const User = require('../../models/user');
 
-
 module.exports.expectJson = (request) => {
-  expect(request.header).to.has.property('content-type');
+  expect(request.header).to.have.property('content-type');
   expect(request.header['content-type']).contains('application/json');
 };
 
 module.exports.createUser = () => {
   return User.create({
-    name: "Manuel",
-    surname: "Amato",
-    email: "manuel.amato@stevejobs.academy",
-    password: "12345"
+    name: 'Manuel',
+    surname: 'Amato',
+    email: 'manuel.amato@stevejobs.academy',
+    password: '12345',
   });
-}
+};
 
 module.exports.createTweet = (user) => {
   return Tweet.create({
     _author: user._id,
-    tweet: "Hello world",
+    tweet: 'Hello world',
   });
-}
+};
+module.exports.deleteTweet = (tweet) => {
+  tweet ? tweet.remove() : console.log('cannot delete tweet');
+  user ? user.remove() : console.log('cannot delete user');
+};
 
-
+module.exports.deleteAll = () => {
+  User.deleteMany({});
+  Tweet.deleteMany({});
+};
